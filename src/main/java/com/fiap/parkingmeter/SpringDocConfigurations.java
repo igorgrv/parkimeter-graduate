b4f57b23ebbe2c69dd2b4672210659bc8cd3a147
alexgrds <133270990+alexgrds@users.noreply.github.com>
2023-11-05 14:32:39 -0300

    Update SpringDocConfigurations.java

--- src/main/java/com/fiap/parkingmeter/SpringDocConfigurations.java
+++ src/main/java/com/fiap/parkingmeter/SpringDocConfigurations.java
@@ -10,7 +10,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "Parking Meter Monitor", version = "0.0.1", description = "Solution for monitoring Parking Meters", license = @License(name = "MIT License", url = "https://github.com/igorgrv/parkingmeter-graduate")), servers = {
         @Server(url = "http://localhost:8080"),
-        @Server(url = "https://localhost:8080")
+        @Server(url = "https://localhost:8080"),
         @Server(url = "https://parkingmeter-e2kw5mlx.b4a.run")
 })
 public class SpringDocConfigurations {
